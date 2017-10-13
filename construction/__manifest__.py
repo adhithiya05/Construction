1c59f5defb898dffe33315012d61d10a89361f5d
Adhithiya R <iluvtechno1@gmail.com>
2017-10-12 23:16:03 -0700

    Update __manifest__.py

--- construction/__manifest__.py
+++ construction/__manifest__.py
@@ -1,16 +1,20 @@
 {
     'name' : 'Construction Management',
     'version' : '1.1',
+    'author' : 'Datayaan',
     'summary': 'Construction Management',
     'description': """
 Construction Management""",
     'category': '',
-    'depends' : ['hr','project'],
+    'depends' : ['hr','purchase'],
     'data': [
         # 'security/student_security.xml',
          # 'security/ir.model.access.csv',
-        # 'update_xml': ['security/ir.model.access.csv1'], 
-        # 'security/ir.model.access.csv',
+        # 'update_xml': ['security/ir.model.access.csv1'],
+        'data/ir_sequence_data.xml', 
+        'security/ir.model.access.csv',
+        'report/project_setup.xml',
+        'report/reports.xml',
         'views/construction.xml',
     ],
     'installable': True,
